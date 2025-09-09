997dd204c0d12d483d1515be0b4d3cff9089fe7e
Joe <hanbiji@163.com>
2025-09-09 23:57:32 +0800

    开始

---
+++ clothing_development_approval/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# 导入模型文件
+from . import clothing_development_request
+# from . import approval_workflow
